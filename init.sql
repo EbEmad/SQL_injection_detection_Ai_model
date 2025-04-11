769fc8866f761145a2dde43e2167cb92d6a92eb0
Ebrahim Emad <ebemad8@gmail.com>
2025-04-11 14:24:54 +0200

    adding init the database

---
+++ init.sql
@@ -0,0 +1,19 @@
+create table if not exists requests(
+    id int auto_increment primary key,
+    source_ip varchar(50) not null,
+    request_type varchar(50) not null,
+    endpoint varchar(255) not null,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    response_time float not null,
+    user_agent text,
+    content_length int,
+    status_code int,
+    referrer text,
+    query_parameters text,
+    headers text,
+    error_message text,
+    api_version varchar(50),
+    client_id VARCHAR(255),
+    request_id VARCHAR(255)
+
+);
